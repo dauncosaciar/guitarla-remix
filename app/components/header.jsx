86fa2175ebb3a05d48262408508a8dac1de3bd43
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-13 16:39:23 -0300

    finishing header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,15 +1,28 @@
-import { Link } from "@remix-run/react";
+import { Link, useLocation } from "@remix-run/react";
+import logo from "../../public/img/logo.svg";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header className="header">
       <div className="contenedor barra">
-        <div className="logo"></div>
+        <Link to="/">
+          <img className="logo" src={logo} alt="Imagen logo" />
+        </Link>
         <nav className="navegacion">
-          <Link to="/">Inicio</Link>
-          <Link to="/nosotros">Nosotros</Link>
-          <Link to="/tienda">Tienda</Link>
-          <Link to="/blog">Blog</Link>
+          <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+            Inicio
+          </Link>
+          <Link to="/nosotros" className={location.pathname === "/nosotros" ? "active" : ""}>
+            Nosotros
+          </Link>
+          <Link to="/tienda" className={location.pathname === "/tienda" ? "active" : ""}>
+            Tienda
+          </Link>
+          <Link to="/blog" className={location.pathname === "/blog" ? "active" : ""}>
+            Blog
+          </Link>
         </nav>
       </div>
     </header>
