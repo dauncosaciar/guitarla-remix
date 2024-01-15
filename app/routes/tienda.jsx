1a50c1f30d3dea473fcbffb670b0f3d16db3efe6
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-15 15:27:10 -0300

    guitars list page finished

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,6 +1,28 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "~/models/guitarras.server";
 import Guitarra from "~/components/guitarra";
+import styles from "~/styles/guitarras.css";
+
+export function meta() {
+  return [
+    {
+      title: "GuitarLA - Tienda de Guitarras"
+    },
+    {
+      name: "description",
+      content: "GuitarLA - Nuestra colección de guitarras"
+    }
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ];
+}
 
 export async function loader() {
   const guitarras = await getGuitarras();
