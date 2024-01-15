b4626493a893eb9845790ad5c3dbb208b2018a44
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-15 12:35:45 -0300

    working on guitars list page

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,13 +11,16 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true,
-    },
+      jsx: true
+    }
   },
   env: {
     browser: true,
     commonjs: true,
-    es6: true,
+    es6: true
+  },
+  globals: {
+    process: true
   },
 
   // Base config
@@ -31,26 +34,26 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
-        "plugin:jsx-a11y/recommended",
+        "plugin:jsx-a11y/recommended"
       ],
       settings: {
         react: {
-          version: "detect",
+          version: "detect"
         },
         formComponents: ["Form"],
         linkComponents: [
           { name: "Link", linkAttribute: "to" },
-          { name: "NavLink", linkAttribute: "to" },
-        ],
-      },
+          { name: "NavLink", linkAttribute: "to" }
+        ]
+      }
     },
 
     // Node
     {
       files: [".eslintrc.js"],
       env: {
-        node: true,
-      },
-    },
-  ],
+        node: true
+      }
+    }
+  ]
 };
