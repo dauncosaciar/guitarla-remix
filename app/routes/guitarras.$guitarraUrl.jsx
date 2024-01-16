bd8e866dfb5f77bd7e888c01e97611f2017f23df
Daniel Alejandro Uncos Aciar <da.uncosaciar@gmail.com>
2024-01-16 20:13:06 -0300

    showing guitar details

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,5 +1,27 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarra } from "~/models/guitarras.server";
+import styles from "~/styles/guitarras.css";
+
+export function meta({ data }) {
+  return [
+    {
+      title: `GuitarLA - ${data.data[0].attributes.nombre}`
+    },
+    {
+      name: "description",
+      content: `Guitarras, venta de guitarras, guitarra ${data.data[0].attributes.nombre}`
+    }
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles
+    }
+  ];
+}
 
 export async function loader({ params }) {
   const { guitarraUrl } = params;
@@ -11,10 +33,23 @@ export async function loader({ params }) {
 
 function Guitarra() {
   const guitarra = useLoaderData();
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
 
-  console.log(guitarra.data[0].attributes);
+  return (
+    <main className="contenedor guitarra">
+      <img
+        className="imagen"
+        src={imagen.data.attributes.url}
+        alt={`Imagen de la guitarra ${nombre}`}
+      />
 
-  return <div>$guitarraUrl</div>;
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
+  );
 }
 
 export default Guitarra;
